60e595f5ad785ac75f88a104af47e91ff6119446
Louis Tsang <kawingt2@gmail.com>
2023-02-23 23:09:42 +0800

    added function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.tgTaskUpdate = functions.firestore
+  .document("/tasks/{taskId}")
+  .onCreate((snap, context) => {
+    const Token = process.env.TG_TOKEN;
+    const id = process.env.CHAT_ID;
+    const bot = new Telegraf(Token);
+    const originalTask = snap.data().original;
+    functions.logger.log("Task created: ", context.params.taskId, originalTask);
+
+    // notify Telegram
+    bot.telegram.sendMessage(id, `New task created: ${originalTask}`);
+  });
